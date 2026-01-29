5ae9f45d40fcc5997e712b487aea00a898a42a7f
siddhartha karre <karresiddhartha56@gmail.com>
2026-01-29 23:19:58 +0530

    Fix hooks folder casing for Linux build

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import { useState } from 'react'
 import {InputBox} from './components'
-import useCurrencyInfo from './hooks/useCurrencyInfo.js'
+import useCurrencyInfo from './Hooks/useCurrencyInfo.js'
 
 
 
